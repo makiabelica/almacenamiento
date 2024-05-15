9bd354ac927fef9b8875576b39edf8827a953ebe
unknown <UFG@CC6-08.ufg.local>
2024-05-14 18:53:56 -0600

    initial project

---
+++ app/src/main/java/edu/sv/ufg/fis/amb/almacenamientointernoexterno/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.sv.ufg.fis.amb.almacenamientointernoexterno
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
